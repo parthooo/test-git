1479bd5e0539d9c192d3d2bba1180a7a73968ce6
parthoooo <partho.rahman@sjinnovation.com>
2024-01-11 20:24:08 +0600

    chagned

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+import getAllUsers from "@/lib/api/getAllUsers";
+import Link from "next/link";
+
 export default async function LifeWithRett() {
-    const content = (
-      <section className="max-container">
-        <br />
-        life with rett
-      </section>
-    );
-  
-    return content;
-  }
+  const usersData = getAllUsers();
+
+  const users = 
+  await usersData;
+
+  const content = (
+    <section>
+      <h2>
+        <Link href="/">
+            Back to Home
+        </Link>
+      </h2>
+      <br />
+      {users.map((user) => {
+        return (
+          <>
+            <p key={user.id}>
+              <Link href={`/users/${user.id}`}>{user.name}</Link>
+            </p>
+            <br />
+          </>
+        );
+      })}
+    </section>
+  );
+
+  return content;
+}
